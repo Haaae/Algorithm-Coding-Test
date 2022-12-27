3ddccf3b882809e74d75d24e066f3c0126f28bf5
Haaae <101785628+Haaae@users.noreply.github.com>
2022-12-27 18:19:43 +0900

    프로그래머스 11047 문제 풀이

--- Greedy/동전0_11047_답안.py
+++ Greedy/동전0_11047_답안.py
@@ -2,7 +2,7 @@ p,*o = open(0)
 # open(0) : https://www.acmicpc.net/board/view/80793
 
 # Asterisk(*) : https://mingrammer.com/understanding-the-asterisk-of-python/
-k = int(p[2:])
+k = int(p[2:]) # [2:]로 k 값에 접근하는 이유는 p == '10 4200'와 같은 str이기 때문이다.
 c = 0
 for i in map(int,o[::-1]):
     c += k // i
